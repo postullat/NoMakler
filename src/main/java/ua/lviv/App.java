d7162b98876ecddcdba56b3440299b2dae79dea7
postullat <postullat@i.ua>
2015-12-07 01:00:18 +0200

    Task #499243 Configured Spring IoC container

--- src/main/java/ua/lviv/App.java
+++ src/main/java/ua/lviv/App.java
@@ -1,5 +1,11 @@
 package ua.lviv;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.applet.AppletContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        TestBean bean = context.getBean("testBean", TestBean.class);
+        System.out.println(bean.getMessage());
+
+        bean.setMessage("Hello nomakler");
+
+        TestBean bean2 = context.getBean("testBean", TestBean.class);
+        System.out.println(bean2.getMessage());
     }
 }
---
+++ src/main/java/ua/lviv/TestBean.java
@@ -0,0 +1,16 @@
+package ua.lviv;
+
+/**
+ * Created by Volodymyr on 07.12.2015.
+ */
+public class TestBean {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
